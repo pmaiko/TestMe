9449287453c78723d0a305a585e6ec625e2d33c5
petyamaiko <petyamaiko@gmail.com>
2024-01-09 01:41:58 +0200

    optimize!!!

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -111,50 +111,24 @@ class TestController extends Controller
 
       $attemptId = $attempt->id;
 
-      $resultAttemptQuestionsData = [];
-      $test->questions->each(function (Question $question) use (&$resultAttemptQuestionsData, $attemptId, $testId, $userId) {
+      $test->questions->each(function (Question $question) use ($attemptId, $testId, $userId) {
         $question_id = $question->id;
 
-        $resultAttemptQuestionsData[] = [
+        $resultAttemptQuestion = ResultAttemptQuestion::query()->create([
           'attempt_id' => $attemptId,
           'test_id' => $testId,
           'question_id' => $question_id,
           'user_id' => $userId,
-          'created_at' => now(),
-          'updated_at' => now()
-        ];
+        ]);
+
+        $question->answers->each(function (Answer $answer) use ($resultAttemptQuestion, $attemptId) {
+          ResultAttemptQuestionAnswer::query()->create([
+            'attempt_id' => $attemptId,
+            'answer_id' => $answer->id,
+            'result_attempt_question_id' => $resultAttemptQuestion->id
+          ]);
+        });
       });
-
-      ResultAttemptQuestion::query()->insert($resultAttemptQuestionsData);
-      $resultAttemptQuestions = ResultAttemptQuestion::query()
-        ->where('attempt_id', $attemptId)
-        ->where('test_id', $testId)
-        ->where('user_id', $userId)
-        ->select('id', 'question_id')
-        ->get();
-
-      $resultAttemptQuestionAnswersData = [];
-      $test->questions->each(function (Question $question) use (&$resultAttemptQuestionAnswersData, $resultAttemptQuestions, $attemptId) {
-        $questionId = $question->id;
-
-        $finedId = optional($resultAttemptQuestions->first(function ($resultAttemptQuestion) use ($questionId) {
-          return (string)($resultAttemptQuestion->question_id) === (string)($questionId);
-        }))->id;
-
-        if (isset($finedId)) {
-          $question->answers->each(function (Answer $answer) use (&$resultAttemptQuestionAnswersData, $finedId, $attemptId) {
-            $resultAttemptQuestionAnswersData[] = [
-              'attempt_id' => $attemptId,
-              'answer_id' => $answer->id,
-              'result_attempt_question_id' => $finedId,
-              'created_at' => now(),
-              'updated_at' => now()
-            ];
-          });
-        }
-      });
-
-      ResultAttemptQuestionAnswer::query()->insert($resultAttemptQuestionAnswersData);
 
       return new BaseJsonResource([
         'questions' => new QuestionCollection($test->questions),
@@ -166,6 +140,51 @@ class TestController extends Controller
         'createdAt' => $test->created_at,
         'updatedAt' => $test->updated_at,
       ]);
+
+      // $resultAttemptQuestionsData = [];
+      // $test->questions->each(function (Question $question) use (&$resultAttemptQuestionsData, $attemptId, $testId, $userId) {
+      //   $question_id = $question->id;
+      //
+      //   $resultAttemptQuestionsData[] = [
+      //     'attempt_id' => $attemptId,
+      //     'test_id' => $testId,
+      //     'question_id' => $question_id,
+      //     'user_id' => $userId,
+      //     'created_at' => now(),
+      //     'updated_at' => now()
+      //   ];
+      // });
+      //
+      // ResultAttemptQuestion::query()->insert($resultAttemptQuestionsData);
+      // $resultAttemptQuestions = ResultAttemptQuestion::query()
+      //   ->where('attempt_id', $attemptId)
+      //   ->where('test_id', $testId)
+      //   ->where('user_id', $userId)
+      //   ->select('id', 'question_id')
+      //   ->get();
+      //
+      // $resultAttemptQuestionAnswersData = [];
+      // $test->questions->each(function (Question $question) use (&$resultAttemptQuestionAnswersData, $resultAttemptQuestions, $attemptId) {
+      //   $questionId = $question->id;
+      //
+      //   $finedId = optional($resultAttemptQuestions->first(function ($resultAttemptQuestion) use ($questionId) {
+      //     return (string)($resultAttemptQuestion->question_id) === (string)($questionId);
+      //   }))->id;
+      //
+      //   if (isset($finedId)) {
+      //     $question->answers->each(function (Answer $answer) use (&$resultAttemptQuestionAnswersData, $finedId, $attemptId) {
+      //       $resultAttemptQuestionAnswersData[] = [
+      //         'attempt_id' => $attemptId,
+      //         'answer_id' => $answer->id,
+      //         'result_attempt_question_id' => $finedId,
+      //         'created_at' => now(),
+      //         'updated_at' => now()
+      //       ];
+      //     });
+      //   }
+      // });
+      //
+      // ResultAttemptQuestionAnswer::query()->insert($resultAttemptQuestionAnswersData);
     }
 
     public function create (Request $request) {
